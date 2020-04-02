5a281df18c5840fbbeda01ebcc588e100ac6608c
Master-Hash <56669192+Master-Hash@users.noreply.github.com>
2020-04-02 23:22:48 +0800

    删去无关代码

--- 进度条.py
+++ 进度条.py
@@ -48,18 +48,3 @@ else:
     print()
 for i in range(100):
     sys.stdout.write('\r%s'%e[random.randrange(0,3)])
-class Student(object):
-    @property
-    def score(self):
-        return self.__score
-
-    @score.setter
-    def score(self, value):
-        if not isinstance(value, int):
-            raise ValueError('score must be an integer!!!')
-        if value < 0 or value > 100:
-            raise ValueError('score must between 0~100!!!')
-        self.__score = value
-a = Student()
-a.score = 66
-a.score += 44
